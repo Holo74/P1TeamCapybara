be39e0f83b04c0f7e75991a80ded89fc753091bd
Holo74 <njk0074@gmail.com>
2024-12-06 12:47:47 -0700

    Created the broken pillar code fully

--- Assets/Scripts/Environment/Interactables/BrokenPillar.cs
+++ Assets/Scripts/Environment/Interactables/BrokenPillar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,69 @@ using UnityEngine;
 
 namespace Environment.Interactables
 {
-    public class BrokenPillar : MonoBehaviour
+    public class BrokenPillar : AShoveable
     {
+        [SerializeField, Tooltip("Direction you must shove the pillar in order for it to collapse.")]
+        private Vector3 pushInDirection;
 
-        private bool canPush { get; set; }
+        [SerializeField, Tooltip("The amount of time that is required for the pillar to be pushed over.")]
+        private float requiredShoveTime;
+        [SerializeField, Tooltip("The time that the player needs to not push the pillar for the time to push to reset.")]
+        private float resetRequiredTime = 1f;
+
+
+        // The magic structure class should go here so that it can be disabled when the pillar is knocked over.
+
+        // How long the player has been shoving the pillar for.
+        private float currentShoveTime = 0f;
+
+        // The duration that the player has not been shoving the pillar for.
+        private float currentResetTime = 0f;
+        // Whether the pillar can be pushed.
+        private bool canShove { get; set; }
+
+
+        void Update()
+        {
+            if (currentShoveTime > 0f && currentResetTime < resetRequiredTime)
+            {
+                currentResetTime += Time.deltaTime;
+                if (currentResetTime > resetRequiredTime)
+                {
+                    currentShoveTime = 0f;
+                }
+            }
+        }
+
 
         public void EnablePushing()
         {
-            canPush = true;
+            canShove = true;
         }
+
+
+
+        public override Vector3 Shoving(Vector3 direction)
+        {
+            if (canShove)
+            {
+                currentShoveTime += Time.deltaTime;
+                currentResetTime = 0f;
+                if (currentShoveTime > requiredShoveTime)
+                {
+                    PushPillar();
+                }
+            }
+            return Vector3.zero;
+        }
+
+        private void PushPillar()
+        {
+            canShove = false;
+            // Disable magic bubble
+        }
+
+
     }
 }
 
